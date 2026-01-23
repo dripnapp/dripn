9ca9d1d98eef4630373e70a23bae0b544f074c72
dripnapp <dripnapp@proton.me>
2026-01-23 15:40:13 +0000

    Fully removed Unity ad

--- unity-ads.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "expo-unity-ads" {
-  export function initialize(gameId: string, testMode: boolean): Promise<void>;
-  export function load(placementId: string): Promise<void>;
-  export function show(placementId: string): Promise<void>;
-  export function setRewardListener(
-    callback: (placementId: string, reward: { amount: number }) => void,
-  ): void;
-}
